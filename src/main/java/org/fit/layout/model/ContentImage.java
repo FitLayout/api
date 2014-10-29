4fa5003c34d17035272ff650836dbefaa9eae703
Radek Burget <burgetr@fit.vutbr.cz>
2014-10-29 11:31:22 +0100

    ContentImage interface

---
+++ src/main/java/org/fit/layout/model/ContentImage.java
@@ -0,0 +1,16 @@
+/**
+ * ContentImage.java
+ *
+ * Created on 29. 10. 2014, 11:19:39 by burgetr
+ */
+package org.fit.layout.model;
+
+/**
+ * An image used as the box content.
+ * 
+ * @author burgetr
+ */
+public interface ContentImage extends ContentObject
+{
+
+}
