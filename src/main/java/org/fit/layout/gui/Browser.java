a7b33b7a36a5b57fea95309b9c032bcf458d2faa
Radek Burget <burgetr@fit.vutbr.cz>
2015-01-23 14:30:46 +0100

    New GUI browser API - basic interfaces

---
+++ src/main/java/org/fit/layout/gui/Browser.java
@@ -0,0 +1,16 @@
+/**
+ * Browser.java
+ *
+ * Created on 23. 1. 2015, 13:36:15 by burgetr
+ */
+package org.fit.layout.gui;
+
+/**
+ * A GUI browser interface used for creating browser plugins.
+ * 
+ * @author burgetr
+ */
+public interface Browser
+{
+
+}
