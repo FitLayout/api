2070b3e2088d7d54607f11c2d9478ed617caa77f
Radek Burget <burgetr@fit.vutbr.cz>
2015-01-24 18:43:06 +0100

    Browser GUI API functions

--- src/main/java/org/fit/layout/gui/Browser.java
+++ src/main/java/org/fit/layout/gui/Browser.java
@@ -5,6 +5,9 @@
  */
 package org.fit.layout.gui;
 
+import javax.swing.JComponent;
+import javax.swing.JToolBar;
+
 /**
  * A GUI browser interface used for creating browser plugins.
  * 
@@ -13,4 +16,24 @@ package org.fit.layout.gui;
 public interface Browser
 {
 
+    /**
+     * Adds a toolbar to the browser's toolbar area.
+     * @param toolbar the toolbar to be added
+     */
+    public void addToolBar(JToolBar toolbar);
+    
+    /**
+     * Adds a new tab to the structure view tabs.
+     * @param title the tab title
+     * @param component the component to be added
+     */
+    public void addStructurePanel(String title, JComponent component);
+    
+    /**
+     * Adds a new panel to the details area.
+     * @param component the component to be added
+     * @param weighty the resizing vertical weight
+     */
+    public void addInfoPanel(JComponent component, double weighty);
+    
 }
