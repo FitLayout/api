c62e6d03e142542fb429aaed75c88c9f3a130d08
Radek Burget <burgetr@fit.vutbr.cz>
2015-01-25 12:02:23 +0100

    Fix hashCode() and equals() in DefaultTag

--- src/main/java/org/fit/layout/impl/DefaultTag.java
+++ src/main/java/org/fit/layout/impl/DefaultTag.java
@@ -71,7 +71,11 @@ public class DefaultTag implements Tag
     @Override
     public int hashCode()
     {
-        return value.hashCode();
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
     }
 
     @Override
@@ -79,7 +83,21 @@ public class DefaultTag implements Tag
     {
         if (this == obj) return true;
         if (obj == null) return false;
-        return value.equals(obj.toString());
+        if (getClass() != obj.getClass()) return false;
+        DefaultTag other = (DefaultTag) obj;
+        if (type == null)
+        {
+            if (other.type != null) return false;
+        }
+        else if (!type.equals(other.type)) return false;
+        if (value == null)
+        {
+            if (other.value != null) return false;
+        }
+        else if (!value.equals(other.value)) return false;
+        return true;
     }
+
+
     
 }
